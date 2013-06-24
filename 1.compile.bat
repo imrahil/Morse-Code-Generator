5c90246d3dfef1e74a8985fef6272df62cece4ef
Jarek Szczepanski <imrahil@imrahil.com>
2013-06-24 20:39:24 +0200

    Updated Morse table image. Removed build scripts from repository.

--- 1.compile.bat
+++
@@ -1,4 +0,0 @@
-cls
-call "%PLAYBOOK_HOME%\bin\amxmlc.bat" -define=CONFIG::debugMode,false -define=CONFIG::device,true -library-path+=libs -external-library-path+="%PLAYBOOK_HOME%\frameworks\libs\qnx\ane\QNXDevice.ane" -output bin-release\MorseCodeGenerator.swf src\MorseCodeGenerator.as
-
-rem -debug=true 
